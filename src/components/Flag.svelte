d240e01f517b9969cf20b3cfa3904590a59ab94d
Galib Anwar <agalib@gmail.com>
2024-09-21 15:10:46 +0600

    Project resize farther adjustments

--- src/components/Flag.svelte
+++ src/components/Flag.svelte
@@ -4,13 +4,13 @@
     export let value, symbol
     let disptcher = new createEventDispatcher()
     function toggle(){
-        value = !value
+        value = !(value + '' == 'true')
         disptcher('change', {value})
         // debugger
     }
 </script>
 
-<span class = "cursor-pointer px-1 rounded" aria-hidden="true" class:selected = {value} on:click={toggle}>{symbol}</span>
+<span class = "cursor-pointer px-1 rounded" aria-hidden="true" class:selected = {value + '' == 'true'} on:click={toggle}>{symbol}</span>
 
 <style>
     .selected {color: green; background-color: bisque;}
